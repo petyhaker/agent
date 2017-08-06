375d477a1090a2931210728be0cf61ef371b3b0a
Vasiliki Petychaki <vasiliki.petychaki@gr.ibm.com>
2017-08-06 19:54:37 +0300

    added agent version 2.0 as a .zip file. Going for agent version 2.1

--- agent_new.pl
+++ agent_new.pl
@@ -8,12 +8,15 @@ getGraph("http://www.beneton.com/")  :- consult(database/benetonGraph).
 getGraph("http://www.skroutz.com/api/") :- consult(database/skroutzGraph).
 
 % explore the availability on all stores
-% check_availability(INTENSION, URLS_LIST, ACCUMULATOR, RESULT)
-check_availability(_, [end_of_file], Result, Result).
-check_availability(Intension, [ A | URLS], Acc, X) :- getGraph(A), write(A), next_state(Intension, entry, A, State),
+% start_traverse(INTENSION, URLS_LIST, ACCUMULATOR, RESULT)
+start_traverse(_, [end_of_file], Result, Result).
+start_traverse(Intension, [ A | URLS], Acc, X) :- getGraph(A), write(A), next_state(Intension, entry, A, State),
 																									path(Intension, State, Request, Method),
 																									agent_inner(Intension, Request, Method, Result, [], State), write(Result),
-																									check_availability(Intension, URLS, [Result|Acc], X).
+																									start_traverse(Intension, URLS, [Result|Acc], X).
+
+check_availability([A|URLS], SearchBy, Value) :- searchProduct(A, SearchBy, Value, ProductInfo).
+
 
 % traverse the graph of 1 store
 % agent_inner(NTENSION, REQUEST, METHOD, RESULT, ACCUMULATOR, STATE)
@@ -26,5 +29,26 @@ agent_inner(Intension, Request, Method, Result, Acc, State) :- path(Intension, S
 
 % starting point
 % start(INTENSION, SCOPE, RESULT) - SCOPE says to the agent whether to search all or a specific store
-start(Intension, all, Result) :- input('database/trusted_stores.txt', URLS), write(URLS), check_availability(Intension, URLS, [], Result).
-start(Intension, Store, Result) :- check_availability(Intension, [Store, end_of_file], [], Result).
+start(buyProduct, all, Result) :- collect_prices(URLS, 'name', 'Moby Dick', [], ProductList), predsort(ProductList, SortedProductList),
+ 																	con
+start(Intension, all, Result) :- input('database/trusted_stores.txt', URLS), write(URLS), start_traverse(Intension, URLS, [], Result).
+start(Intension, Store, Result) :- start_traverse(Intension, [Store, end_of_file], [], Result).
+
+% lets assume that the search is being done by the server and I only get the json for specific product
+
+searchProduct("http://www.amazon.com/api/", 'name', 'Moby Dick', "database/amazonDB.json").
+
+% go here id Intension==buyProduct & Scope==all
+collect_prices([end_of_file],  _, _, Result, Result).
+collect_prices([A|URLS], SearchBy, Value, Acc, Result) :- not(searchProduct(A, SearchBy, Value, ProductInfo)),
+																						collect_prices(URLS, SearchBy, Value, Acc, Result).
+collect_prices([A|URLS], SearchBy, Value, Acc, Result) :- searchProduct(A, SearchBy, Value, ProductInfo),
+																					 open(ProductInfo, read, Str), json_read_dict(Str,Pr), close(Str)
+ 																					 Pr.offers.avalability == "no",  collect_prices(URLS, SearchBy, Value, Acc, Result).
+collect_prices([A|URLS], SearchBy, Value, Acc, Result) :- searchProduct(A, SearchBy, Value, ProductInfo),
+																						open(ProductInfo, read, Str), json_read_dict(Str,Pr), close(Str), atom_number(Pr.offers.price, Price),
+																						collect_prices(URLS, SearchBy, Value, [[Price, A]|Acc], Result).
+
+
+
+compareAvg(X,  [A1, _], [A2, _]) :- compare(X, A1, A2).
--- test/test.pl
+++ test/test.pl
@@ -1,9 +1,10 @@
 :- use_module(library(http/json)).
 :- use_module(library(http/json_convert)).
 
-in_json(File, Result) :-
+in_json(File, SearchBy, Value, Result) :-
     open(File, read, Str),
-    json_read(Str,Res),
-    write(Res.'Amazon'),
-    json_to_prolog(Res, Result),
+    json_read_dict(Str,Result),
+    Result.offers.availability =="yes",
     close(Str), nl, !.
+
+compareAvg(X,  [_,A1], [_,A2]) :- compare(X, A1, A2).
